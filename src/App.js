11ab79207784996ccc5af54b98c709095bc29d1f
atatum718 <54254894+atatum718@users.noreply.github.com>
2020-10-11 18:08:46 -0400

    added switch, and links to each screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from "react";
+import { Route, Switch, Link } from "react-router-dom";
+
 // import "./App.css";
 import Filter from "./filter/Filter";
 import Gallery from "./gallery/Gallery";
 import Preview from "./preview/Preview";
 
+function Header() {
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/gallery">Gallery</Link>
+      <Link to="/preview">Preview</Link>
+      <Link to="/filter">Filter</Link>
+    </>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <Gallery />
-      <Preview />
-      <Filter />
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Header} />
+        <Route path="/gallery" component={Gallery} />
+        <Route path="/preview" component={Preview} />
+        <Route path="/filter" component={Filter} />
+      </Switch>
     </div>
   );
 }
--- src/mainStyles/AppStyles.js
+++ src/mainStyles/AppStyles.js
@@ -1,6 +1,5 @@
 import React from "react";
-import styled from "styled-components";
-import { css } from "styled-components";
+import styled, { css } from "styled-components";
 
 const Button = styled.button`
   background: #fad200;
