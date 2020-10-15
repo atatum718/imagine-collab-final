117e2093463e23b261d05fef555f2f2939a92aa6
Yasir <Yasir.A.Kulane@gmail.com>
2020-10-15 03:10:25 -0500

    filters work, functionality for the app is done

--- src/filter/Filter.js
+++ src/filter/Filter.js
@@ -1,104 +1,131 @@
-import './filter.css';
-import React, { Component } from 'react';
-import { Stage, Layer, Image } from 'react-konva';
-import { Route, Switch, Link } from "react-router-dom";
-import Konva from 'konva';
-import useImage from 'use-image';
+import "./filter.css";
+import React, { Component } from "react";
+import Konva from "konva";
+import { Stage, Layer, Image } from "react-konva";
+import useImage from "use-image";
 
+const ImageViewer = (props) => {
+  const [image] = useImage(props.fileUrl, 'Anonimus');
+  const imageRef = React.useRef();
 
-class Filter extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      file_url: null,
-      slider_value: 0,
+  // when image is loaded we need to cache the shape
+  React.useEffect(() => {
+    if (image) {
+      // you many need to reapply cache on some props changes like shadow, stroke, etc.
+      imageRef.current.cache();
+      // since this update is not handled by "react-konva" and we are using Konva methods directly
+      // we have to redraw layer manually
+      imageRef.current.getLayer().batchDraw();
     }
-  }
-  
-handleChange = (event) => {
-  this.setState({
-    file_url: URL.createObjectURL(event.target.files[0]),
-  });
-}
+  }, [image]);
 
-handleSlider = (event) => {
-  this.setState({
-    slider_value: event.target.value - 50,
-  });
-}
-  
-    render() {
-      return ( 
-      <>
-        <div className="filter-screen-container">                        
-          <div class="contrast">
-            <Stage width={window.innerWidth - 400} height={window.innerHeight - 400}>
-              <Layer>
-                <FilterImage file_url={this.state.file_url} value={this.state.slider_value} />
-              </Layer>
-            </Stage>
-          </div>
+  //console.log(JSON.stringify(props));
 
-          
-          {/* <input className="input1" type="file" onChange={this.handleChange}/> */}
-          
-          <p>{this.state.slider_value}</p>
-          <input type="range" onChange={(event) => this.handleSlider(event)} />
-  
-        </div>
-        <div className="button5"> <input className="input1" type="file" onChange={this.handleChange}/> </div>
-      </>
-      );
-      
-    }
-}
-
-class FilterImage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    }; 
-
-    // Fix this too, need to create an image object to render to the Konva Image tag
-    // React is throwing errors though because of where its being run
-
-      // const [image] = useImage(props.file_url);
-  }
-
-    /*
-    Props transfer over successfully
-    File url for uploaded image world properly
-    Having issues with React.useEffect and React.useRef()
-    Unable to apply changes to the image due to bad ref cache
-    Error is "imageRef.current is undefined"
-    Everything else seems to be working OK for now
-    */
-  
-    // Sample code found on the Konva docs below \/ \/ \/
-    // when image is loaded we need to cache the shape
-    // React.useEffect(() => {
-    //   if (image) {
-    //     // you many need to reapply cache on some props changes like shadow, stroke, etc.
-    //     imageRef.current.cache();
-    //     // since this update is not handled by "react-konva" and we are using Konva methods directly
-    //     // we have to redraw layer manually
-    //     imageRef.current.getLayer().batchDraw();
-    //   }
-    // }, [image]);
-  
-    render() {
-      return (
-        <Image
-          x={10}
-          y={10}
-          image={this.state.image}
-          filters={[Konva.Filters.Blur]}
-          blurRadius={this.props.value}
-        />
-      );
-    };
+  return (
+    <Image
+      ref={imageRef}
+      x={100}
+      y={100}
+      image={image}
+      filters={[Konva.Filters.HSV]}
+      saturation={props.saturation / 100}
+      value={props.contrast / 100}
+      shadowBlur={5}
+    />
+  );
+  // https://konvajs.org/api/Konva.Filters.html#.HSV
 };
 
 
+
+class ImageControls extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleContSlider = (event) => {
+    this.props.handleContrastUpdate((event.target.value - 50) * 2);
+  };
+
+  handleSatSlider = (event) => {
+    this.props.handleSaturationUpdate((event.target.value - 50) * 2);
+  };
+
+
+
+  render() {
+    return (
+      <div>
+        <p>Contrast: {this.props.contrastVal}</p>
+        <input type="range" onChange={(event) => this.handleContSlider(event)} />
+        <p>Saturation: {this.props.saturationVal}</p>
+        <input type="range" onChange={(event) => this.handleSatSlider(event)} />
+        <br/>
+        <br/>
+        <br/>
+        
+        <input
+          className="input1"
+          type="file"
+          onChange={(event) => this.props.handleUpload(event)}
+        />
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  state = {
+    contrastVal: 0,
+    saturationVal: 0,
+    fileUrl: new URL("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTc111TqOByv02UVqoDNIgnhzyfPG7Fefd5-w&usqp=CAU"),
+  };
+
+  handleUpload = (event) => {
+    this.setState({
+      fileUrl: URL.createObjectURL(event.target.files[0]),
+    });
+  };
+
+  handleContrastUpdate = (value) => {
+    this.setState({
+      contrastVal: value,
+    });
+  };
+
+  handleSaturationUpdate = (value) => {
+    this.setState({
+      saturationVal: value,
+    });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Stage
+          width={window.innerWidth / 2}
+          height={window.innerHeight / 2}
+        >
+          <Layer>
+            <ImageViewer 
+            contrast={this.state.contrastVal}
+            saturation={this.state.saturationVal}
+            fileUrl={this.state.fileUrl} />
+          </Layer>
+        </Stage>
+        <ImageControls
+          contrastVal={this.state.contrastVal}
+          handleContrastUpdate={this.handleContrastUpdate}
+
+          saturationVal={this.state.saturationVal}
+          handleSaturationUpdate={this.handleSaturationUpdate}
+
+          handleUpload={this.handleUpload}
+        />
+      </div>
+    );
+  }
+}
 
 export default Filter;
