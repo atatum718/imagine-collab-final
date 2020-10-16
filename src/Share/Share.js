adacf22b22b15e8d45c5432a9638ecc72d5a6362
imjaylenbro <55603069+imjaylenbro@users.noreply.github.com>
2020-10-16 09:14:32 -0700

    added content to the share screen

--- src/Share/Share.js
+++ src/Share/Share.js
@@ -1,18 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./share-screen.css";
+import { FontAwesomeIcon } from "@fortawesome/reat-fontawesome";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
-function Share (){
-  
-    return (
-      <div>
-        <div>
-        <img src="./Img/facebook.png" alt=""/>
-         </div>
-         <div>
-             <img src="./Img/twitter.png" alt=""/>
-         </div>
+const arrow = <FontAwesomeIcon icon={faAngleRight} size="1x" color="#0ECAF5" />;
+
+function Share() {
+  return (
+    <div className="container">
+      <div className="box">
+        <ul className="ul-list">
+          <li>
+            <img src="./src/Share/img/facebook.png" alt="facebook" />
+          </li>
+          <li>
+            <p className="arrow">{arrow} </p>
+          </li>
+        </ul>
       </div>
-    );
-  
+      <div className="box">
+        <ul className="ul-list">
+          <li>
+            <img src="./src/Share/img/twitter.png" alt="twitter" />
+          </li>
+          <li>
+            <p className="arrow">{arrow} </p>
+          </li>
+        </ul>
+      </div>
+
+      <div className="box">
+        <ul className="ul-list">
+          <li>
+            <img src="./src/Share/img/intsa.png" alt="intsagram" />
+          </li>
+          <li>
+            <p className="arrow">{arrow} </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 export default Share;
